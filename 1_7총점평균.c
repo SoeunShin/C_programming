5e476f351fb3bc33bab299522b7c84fae99bce41
Soeun Shin <61735948+SoeunShin@users.noreply.github.com>
2022-03-02 19:24:25 +0900

    220302

---
+++ 1_7총점평균.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+  int kor, eng, math;   // 국어점수, 영어점수, 수학점수 
+  int total;            // 총점
+  float average;        // 평균점수 
+  
+  printf("Kor? ");
+  scanf("%d", &kor);
+  printf("Eng? ");
+  scanf("%d", &eng);
+  printf("Math? ");
+  scanf("%d", &math);
+
+  total = kor + eng + math;
+  average = total / 3.0;
+
+  printf("total is %d, average is %.1f.\n", total, average);
+  return 0;
+}

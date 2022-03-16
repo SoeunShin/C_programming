4b9724b515fefc255aae5bff3517ed23e4863b8e
Soeun Shin <61735948+SoeunShin@users.noreply.github.com>
2022-03-16 23:36:08 +0900

    220316

--- 3-6좌표구하기.c
+++ 3-6좌표구하기.c
@@ -5,8 +5,7 @@
   3. x좌표 시작 값부터 끝 값까지 반복 -> x로 둔다.
     3-1. y = ax^2 + bx + c 계산
     3-2. 좌표 (x,y) 출력
-  */
-
+ */
 int main(void) {
   int a, b, c;          // 2차 함수의 계수 a, b, c
   int x_begin, x_end;   // x좌표의 시작 값과 끝 값 

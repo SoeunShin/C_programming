a3b2b3abdac0ac0e9292fe4d8a6593216d4821d5
Soeun Shin <61735948+SoeunShin@users.noreply.github.com>
2022-03-31 21:12:37 +0900

    2200331

---
+++ 5-3점수평균_배열.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/*
+  1. 10번 반복
+    1-1. 심사점수 물어보고 입력받기
+    1-2. total에 현재 입력받은 점수 더하기
+    1-3. max보다 크면 max값 수정
+    1-4. min보다 작으면 min값 수정
+  2. total에서 max와 min 빼기
+  3. avg = total / 8
+  4. 결과 출력
+*/
+int main(void) {
+  float score[10];      // 심사점수
+  float max=0, min=10;       // 가장 큰 점수, 가장 작은 점수
+  float total=0;          // 점수 총 합계
+  float average;        // 평균점수
+  int i;                // 반복문을 위한 변수
+
+  for(i=0; i<10; i++){
+    printf("%d번 심사점수? ", i+1);
+    scanf("%f", &score[i]);
+    total += score[i];
+    if(score[i] > max) max = score[i];
+    if(score[i] < min) min = score[i];
+  }
+  total -= (max + min);
+  average = total/8;
+  printf("가장 큰 점수와 가장 작은 점수를 제외한 8개의 점수에 대한 평균은 %.1f입니다.\n", average);
+  return 0;
+}
---
+++ 5-4두번째큰수_배열.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/*
+  1. 10번 반복
+    1-1. 숫자 물어보고 입력받기 
+    1-2. 입력받은 수가 first보다 크면 first를 수정
+  2. 10번 반복
+    2-1. 입력받은 수가 first보다 작고 second보다 크면 second 수정, index 저장
+  3. 두번째로 큰 수의 index, 숫자 출력
+*/
+int main(void) {
+  int num[10];             // 10개의 숫자
+  int first = 0;               // 첫 번째로 큰 수
+  int second = 0;              // 두 번째로 큰 수
+  int second_max_index;    // 두 번째로 큰 수의 인덱스 
+  int i;                   // 반복문을 위한 변수
+
+  for(i=0; i<10; i++){
+    printf("%d번째 수를 입력하시오. ", i+1);
+    scanf("%d", &num[i]);
+    if(num[i] > first) first = num[i];
+  }
+  for(i=0; i<10; i++){
+    if(num[i] < first && num[i] > second){
+      second_max_index = i+1;
+      second = num[i];
+    }
+  }
+  printf("두번째로 큰 수는 %d번째 수 %d입니다.\n", second_max_index, second);
+  return 0;
+}
